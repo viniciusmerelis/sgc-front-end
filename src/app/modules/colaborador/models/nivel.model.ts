8fe88f842565cc8d9583a9ffc4f5ed01674c2b68
Vinícius Merelis <vinicius.merelis@gmail.com>
2022-03-21 21:42:56 -0300

    Finalizando Colaborador Component

--- src/app/modules/colaborador/models/nivel.model.ts
+++ src/app/modules/colaborador/models/nivel.model.ts
@@ -1,15 +1,15 @@
 import { SelectItem } from "primeng";
 
-export class Nivel {
-    static CONHECE = 'CONHECE';
-    static SABE_APLICAR = 'SABE_APLICAR';
-    static SABE_ENSINAR = 'SABE_ENSINAR';
+export enum Nivel {
+    CONHECE = 'CONHECE',
+    SABE_APLICAR = 'SABE_APLICAR',
+    SABE_ENSINAR = 'SABE_ENSINAR'
+}
 
-    static selectItem: SelectItem[] = [
-        {
-            value: Nivel.CONHECE,
-            label: 'Conhece'
-        },
+export class NivelUtil {
+
+    static selectItems: SelectItem[] = [
+        { value: Nivel.CONHECE, label: 'Conhece' },
         {
             value: Nivel.SABE_APLICAR,
             label: 'Sabe Aplicar'
@@ -18,5 +18,13 @@ export class Nivel {
             value: Nivel.SABE_ENSINAR,
             label: 'Sabe Ensinar'
         }
-    ]
+    ];
+
+    static getLabel(nivel: Nivel): string {
+        const item = NivelUtil.selectItems.find(item => item.value == nivel)
+        if (item) {
+            return item.label
+        }
+        return '' + nivel;
+    }
 }
